24b7863889a638a1b1b320ec5fdf3c4c13bada40
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-05-28 14:47:26 +0200

    (Dependencies) Upgrade to angular v7 lib

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,3 @@
-/* const requireWithoutCache = (name) => {
-	let p = require.resolve(name);
-	if (require.cache[p]) {
-			delete require.cache[p]
-	}
-	return require(name)
-} */
-
 export default class YourAddon {
     public static displayName = 'Your Package';
     public static logo = 'https://s3.amazonaws.com/kinlane-productions/api-evangelist/noun-project/the-noun-project-clicking-heels.jpg';
