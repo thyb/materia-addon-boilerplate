84ff6a0bf35576afb58cfbb81c563dcfaae517c8
GeoAstronaute <jeanmarc.royy@gmail.com>
2020-02-19 18:18:43 +0100

    deps: upgrade @angular/material to v9

--- client/addon-boilerplate.module.ts
+++ client/addon-boilerplate.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {
-  MatButtonModule,
-  MatRippleModule,
-  MatSnackBarModule,
-  MatCardModule,
-  MatIconModule,
-  MatDialogModule,
-  MatInputModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { Addon } from '@materia/addons';
 
